0b6eb8c77fa0cf79ee2ec20fa036b3c1b3f5d29c
Emerson Duarte <pduarte.emerson@gmail.com>
2021-08-23 22:41:49 -0300

    creates components Sidebar and Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
-}
+import { MovieCard } from "./MovieCard";
+
+interface IContent {
+  titleHeader: string,
+  items: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }[],
+};
+
+export function Content({ titleHeader, items }: IContent) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {titleHeader}</span></span>
+      </header>
+      <main>
+        <div className="movies-list">
+          {items.map(item => (
+            <MovieCard key={item.imdbID} title={item.Title} poster={item.Poster} runtime={item.Runtime} rating={item.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
-}
+import { Button } from '../components/Button';
+
+export type ISideBar = {
+  items: {
+      id: number;
+      name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+      title: string;
+    }[],
+  handleClickButton: (id: number) => void,
+  selectedItem: number,
+};
+
+export function SideBar({items, handleClickButton, selectedItem}: ISideBar) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {items.map(item => (
+        <Button
+          key={String(item.id)}
+          title={item.title}
+          iconName={item.name}
+          onClick={() => handleClickButton(item.id)}
+          selected={selectedItem === item.id}
+        />
+        ))}
+      </div>
+    </nav>
+  );
+};
